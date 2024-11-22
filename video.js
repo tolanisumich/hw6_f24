b5ccabc4465b5e96144cb4a886cc98d0b92fbc90
Tolani Smith <tolanism@umich.edu>
2024-11-21 19:51:15 -0500

    Add files via upload

---
+++ video.js
@@ -0,0 +1,63 @@
+var video;
+
+window.addEventListener("load", function() {
+	console.log("Good job opening the window")
+	video = document.querySelector("#player1");
+	video.autoplay = false;
+	video.loop = false;
+	console.log("this works");
+
+	document.querySelector("#play").addEventListener("click", function() {
+		console.log("Play Video");
+		document.querySelector("#volume").innerHTML = video.volume * 100 + "%";
+		video.play();
+	});
+
+	document.querySelector("#pause").addEventListener("click", function() {
+		console.log("Pause Video");
+		document.querySelector("#volume").innerHTML = video.volume * 100 + "%";
+		video.pause();
+	});
+
+	document.querySelector("#slower").addEventListener("click", function() {
+		console.log("Slow Down Video");
+		video.playbackRate *= 0.90;
+		console.log("New speed is " + video.playbackRate);
+	});
+
+	document.querySelector("#mute").addEventListener("click", function() {
+		console.log("Mute Video");
+		if (video.muted == true) {
+			video.muted = false;
+			document.querySelector("#mute").innerHTML = "Mute";
+		} else {
+			video.muted = true;
+			document.querySelector("#mute").innerHTML = "Unmute";
+		}
+	});
+	document.querySelector("#slider").addEventListener("click", function() {
+		console.log("Changing volume");
+		video.volume = this.value / 100;
+		document.querySelector("#volume").innerHTML = video.volume * 100 + "%";
+	});
+	document.querySelector("#faster").addEventListener("click", function() {
+		console.log("Speed Up Video");
+		video.playbackRate /= 0.90;
+		console.log("New speed is " + video.playbackRate);
+	});
+
+	document.querySelector("#skip").addEventListener("click", function() {
+		console.log("Skip Ahead");
+		if (video.currentTime + 15 > video.duration) {
+			video.currentTime = 0;
+		} else {
+			video.currentTime += 15;
+		}
+		console.log("New time is " + video.currentTime);
+	});
+});
+
+// document.querySelector("#play").addEventListener("click", function() {
+// 	console.log("Play Video");
+// });
+
